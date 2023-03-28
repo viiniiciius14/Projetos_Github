ec33439f4ec3c737d7bdd467d07c6428de50f545
vini <viinii_hard14@hotmail.com>
2023-03-27 21:15:18 -0300

    Atualização Projeto Portifolio.
    adicionando um menu com interações em java script básico.

---
+++ HTML-CSS/Projeto-Portifolio/script.js
@@ -0,0 +1,16 @@
+const btn_menu = document.getElementById('btn_menu');
+const hamburguer = document.getElementById('hamburguer');
+
+function toggleMenu() {
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+}
+
+function toggleHam() {
+  const header = document.getElementById('header');
+  header.classList.toggle('active');
+}
+
+
+btn_menu.addEventListener('click', toggleMenu);
+hamburguer.addEventListener('click', toggleHam);
