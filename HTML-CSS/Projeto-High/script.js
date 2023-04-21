bad5752457e93428b162a0b660e669f2fb73b548
vini <viinii_hard14@hotmail.com>
2023-04-20 22:38:00 -0300

    Update!

--- HTML-CSS/Projeto-High/script.js
+++ HTML-CSS/Projeto-High/script.js
@@ -6,15 +6,16 @@ var ul = document.getElementById('ul');
 btn_menu.onclick = function(){
   btn_menu.classList.toggle('active_1');
   if (btn_menu.classList.contains('active_1')) {
-    menu.style.visibility = 'visible'
-    menu.style.maxHeight = '300px'
-    menu.style.width = '100%'
+    menu.style.visibility = 'visible';
+    menu.style.maxHeight = '300px';
+    menu.style.width = '100%';
     menu.style.transition = '.5s ease'
-    ul.style.visibility = 'visible'
+    ul.style.visibility = 'visible';
+    ul.style.transition = '0.1s ease'
     
   }else {
-    ul.style.visibility = 'hidden'
-    menu.style.maxHeight = '0px'
+    ul.style.visibility = 'hidden';
+    menu.style.width = '0%'
   }
   
 }
