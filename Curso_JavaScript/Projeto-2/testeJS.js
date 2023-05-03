9725a9341fddb987fe8bfd1562fd4b927ed0cc36
vini <viinii_hard14@hotmail.com>
2023-05-03 11:40:25 -0300

    Update!

--- Curso_JavaScript/Projeto-2/testeJS.js
+++ Curso_JavaScript/Projeto-2/testeJS.js
@@ -3,36 +3,36 @@ function submitDados(event) {
         event.preventDefault();
     }
 
-    const nameInput = document.getElementById('name');
-    const emailInput = document.getElementById('email');
-    const phoneInput = document.getElementById('phone');
+    const name = document.getElementById('name');
+    const email = document.getElementById('email');
+    const phone= document.getElementById('phone');
     const btn_concluido = document.getElementById('submit');
     const span = document.querySelectorAll('span');
 
     let msgError = false;
     
-    if (nameInput.value.length < 5){ 
-        nameInput.placeholder = 'Digite seu nome completo';
-        nameInput.style.borderColor = 'red';
-        nameInput.style.transition = '.4s ease';
+    if (name.value.length < 5){ 
+        name.placeholder = 'Digite seu nome completo';
+        name.style.borderColor = 'red';
+        name.style.transition = '.4s ease';
         span.forEach(item => item.style.color = '#e60808b0');
         msgError = true;
     }
 
     const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-    if (!emailRegex.test(emailInput.value)) {
-        emailInput.placeholder = 'Email inválido';
-        emailInput.style.borderColor = 'red';
-        emailInput.style.transition = '.4s ease';
+    if (!emailRegex.test(email.value)) {
+        email.placeholder = 'Email inválido';
+        email.style.borderColor = 'red';
+        email.style.transition = '.4s ease';
         span.forEach(item => item.style.color = '#e60808b0');
         msgError = true;
     }
 
     const phoneRegex = /^\d{11}$/;
-    if (!phoneRegex.test(phoneInput.value)){
-        phoneInput.placeholder = 'Número inválido';
-        phoneInput.style.borderColor = 'red';
-        phoneInput.style.transition = '.4s ease';
+    if (!phoneRegex.test(phone.value)){
+        phone.placeholder = 'Número inválido';
+        phone.style.borderColor = 'red';
+        phone.style.transition = '.4s ease';
         span.forEach(item => item.style.color = '#e60808b0');
         msgError = true;
     }
@@ -46,9 +46,9 @@ function submitDados(event) {
     }else {
         const time1 = 2000;
         setTimeout(function() {
-            nameInput.style.borderColor = '#313131';
-            emailInput.style.borderColor = '#313131';
-            phoneInput.style.borderColor = '#313131';
+            name.style.borderColor = '#313131';
+            email.style.borderColor = '#313131';
+            phone.style.borderColor = '#313131';
             span.forEach(item => item.style.color = '#00a2ff')
 
             btn_concluido.value = 'Concluido'
@@ -64,12 +64,11 @@ function submitDados(event) {
         const finish = document.getElementById('finish');
         const form = document.getElementById('form');
         setTimeout(function() {
-            form.style.visibility = 'hidden'
-            finish.style.visibility = 'visible';
-            msg_finish.innerHTML = `Muito bem ${nameInput.value}, seu cadastro foi concluido.`;
-            
+            form.style.display = 'none';
+            finish.style.display = 'flex';;
+            msg_finish.innerHTML = `Muito bem ${name.value}, seu cadastro foi concluido.`;
         }, time2)
     }
-    console.log(`${nameInput.value}, ${emailInput.value}, ${phoneInput.value}`);
+    console.log(`${name.value}, ${email.value}, ${phone.value}`);
 }
 submitDados()
